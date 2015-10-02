e6bc5051605e198e19022a580ee9f0f98277699a
Bin Wu <Bingo@binwus-macbook-air.local>
2015-10-02 08:23:41 +0800

    Initial Commit

---
+++ CPUType/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CPUType
+//
+//  Created by BinWu on 10/2/15.
+//  Copyright © 2015 BinWu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
